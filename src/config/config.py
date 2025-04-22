d7cc253c52b4b3988b7d870b6ad063f19d55eeed
Maxim Degi <unliex@yandex.ru>
2025-04-22 22:00:12 +0300

    temp switch from redis to base

--- src/config/config.py
+++ src/config/config.py
@@ -1,14 +1,16 @@
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
-from aiogram.fsm.storage.redis import RedisStorage, Redis
+# from aiogram.fsm.storage.redis import RedisStorage, Redis
+from aiogram.fsm.storage.memory import MemoryStorage
 from environs import Env
 
 
 env =  Env()
 env.read_env()
 BOT_TOKEN = env('BOT_TOKEN')
-ADMIN_IDS = env.list('ADMIN_IDS')
+ADMIN_IDS: list = env.list('ADMIN_IDS')
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
-redis = Redis(host='localhost')
-storage = RedisStorage(redis=redis) 
+# redis = Redis(host='localhost')
+# storage = RedisStorage(redis=redis)
+storage = MemoryStorage() 
 dp = Dispatcher(storage=storage)
