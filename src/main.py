22ba72ee427c3083d9153d2496c20ab30edf5407
Maxim Degi <unliex@yandex.ru>
2025-04-22 10:37:50 +0300

    and removed debugs from docker

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-print("1")
 import asyncio
 from config.config import bot, dp
 from keyboards.set_menu import set_main_menu
@@ -7,13 +6,9 @@ from middlewares.middlewares import DataBaseAccessor
 
 async def main() -> None:
     await bot.delete_webhook(drop_pending_updates = True)
-    print("9")
     await set_main_menu(bot)
-    print("10")
     dp.include_router(other_handlers.rt)
-    print("13")
     dp.include_router(game.rt)
-    print("15")
     dp.update.middleware(DataBaseAccessor())
     print("starting")
     await dp.start_polling(bot)
