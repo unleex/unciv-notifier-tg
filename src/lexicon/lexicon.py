a9a5c7726169b8a792082132e933ed87ed678c94
Maxim Degi <unliex@yandex.ru>
2025-04-22 22:43:44 +0300

    a

--- src/lexicon/lexicon.py
+++ src/lexicon/lexicon.py
@@ -2,8 +2,10 @@ LEXICON_EN = {
     "init_game": "Paste your game id.",
     "invalid_game_id": "Invalid game id!",
     "waiting_for_assigning_civs": "Select your civs!",
+    "manually_assigned_civ": "Selected %s as player of %s",
+    "invalid_civ": "Invalid civ: %s!",
     "starting": "❇️Starting!",
     "notification": "▶️Turn %d. @%s, move!",
     "notification_no_turns": "▶️Waiting for the first turn...",
-    "stopping": "⏹️Stopping notifications. Good game!"
+    "stopping": "⏹️Stopping notifications. Good game!",
     }
