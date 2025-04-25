ba9976376d18997fc0b385502068adb12037dcc8
Maxim Degi <unliex@yandex.ru>
2025-04-25 08:08:38 +0300

    added manual turn getting, asked model to comment on all news in system prompt

--- src/keyboards/set_menu.py
+++ src/keyboards/set_menu.py
@@ -6,5 +6,6 @@ async def set_main_menu(bot: Bot) -> None:
     main_menu_commands = [
         BotCommand(command="start", description="Start notifications!"),
         BotCommand(command="stop", description="Stop notifications"),
+        BotCommand(command="get_turn", description="Get current turn"),
         ]
     await bot.set_my_commands(main_menu_commands)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from config.config import bot, dp
-from handlers import game, other_handlers
+from handlers import init_game, other_handlers
 from keyboards.set_menu import set_main_menu
 from middlewares.middlewares import DataBaseAccessor
 
@@ -9,7 +9,7 @@ async def main() -> None:
     await bot.delete_webhook(drop_pending_updates = True)
     await set_main_menu(bot)
     dp.include_router(other_handlers.rt)
-    dp.include_router(game.rt)
+    dp.include_router(init_game.rt)
     dp.update.middleware(DataBaseAccessor())
     print("starting")
     await dp.start_polling(bot)
